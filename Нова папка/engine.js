77f770c4bd3b7fb7c2e872f6702c9c9f6d411a67
Georgi5204 <petrovw_georgi@abv.bg>
2021-07-30 20:11:57 +0300

    update

--- Нова папка/engine.js
+++ Нова папка/engine.js
@@ -1,21 +1,19 @@
-         function showBars(){
-            let x = document.getElementById("position").value
-            let text;
-            if (isNaN(x) || x < 1 || x > 100){
-                text = "Please enter a meters between 1 and 100";
+         // function showBars(){
+         //    let x = document.getElementById("position").value
+         //    let text;
+         //    if (isNaN(x) || x < 1 || x > 100){
+         //        //text = "Please enter a meters between 1 and 100";
+         //        console.log("hellooo?");
                
-            } else {
-               
-            
-             document.getElementById("btnOne").style.visibility = "visible";
-             
-
-            }
-            document.getElementById("demo").innerHTML = text;
-            }
+         //    } else {
+         //     document.getElementById("NightBars").style.visibility = "visible"; 
+         //    }
+         //    document.getElementById("demo").innerHTML = text;
+         //    }
          function startTime() {
             let timeText = "";
             let x2 = document.getElementById("position").value
+
             const today = new Date();
             let h = today.getHours();
             let m = today.getMinutes();
@@ -25,20 +23,20 @@
             document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
             setTimeout(startTime, 1000);
 
-            if(isNaN(x2) || x2 < 1 || x2 > 100){
-                timeText = "Please enter a meters between 1 and 100";
+            if(isNaN(x2) || x2 < 1 || x2 > 1000){
+                document.getElementById("bars").style.visibility ='hidden';
+                document.getElementById("NightBars").style.visibility = "hidden";
+                document.getElementById("dailyBars").style.visibility = "hidden";
+                return 0;
+                
             }else {
                     if (h > 19) {
-               //document.getElementById("btn").style.visibility = "visible"; 
                document.getElementById("bars").style.visibility ='visible';
                document.getElementById("NightBars").style.visibility = "visible";
-            } else {
-                //document.getElementById("btn").style.visibility = "visible"; 
-                //document.getElementById("btnOne").style.visibility = "visible";
+               
+            } else {  
                document.getElementById("bars").style.visibility ='visible';
-               document.getElementById("dailyBars").style.visibility = "visible";
-            //console.log('here ?')
-                
+               document.getElementById("dailyBars").style.visibility = "visible";                 
             }
             }
         }
@@ -49,25 +47,13 @@
          function showOpenNightBars(){  
                 document.getElementById("NightBars").style.visibility = "visible"; 
                 
-
-                    //document.getElementsByClassName("NightBars").style.visibility = "visible";
-                    
-                   // var find = document.getElementById("Jar");
-                    // console.log(find);
-                    // if (document.getElementById("Jar").value = "Jar") {
-                    //      console.log('The Jar ');
-                    //     return
-                    // } if (document.getElementById("Rock").value = "Rock") {
-                    //      console.log('rock club ');
-                    //     return
-                    // } 
          }
          function onchangeNightBars(){
+                        document.getElementById("NightBars").style.visibility = "visible"; 
                         var mylist = document.getElementById("NightBar");
                             
                             document.getElementById("demo2").value = NightBar.options[NightBar.selectedIndex].text;
                             if (document.getElementById("NightBar").value == "Jar") {
-                                console.log("Jar club");
                                 document.getElementById("JarPic").style.visibility = "visible";
                                 return
                             } else {
@@ -84,10 +70,43 @@
                                 console.log("rock club");
                                 return
                             } else {
-                                console.log(" :) ");
+                                
                             }
                     }   
-         function showOpenDailyBars(){
-                document.getElementById("dailyBars").style.visibility = "visible";
+         function onchangeDayBars(){
+                var mylist2 = document.getElementById("dailyBar");
+                    document.getElementById("demo3").value = dailyBar.options[dailyBar.selectedIndex].text;
+                    if (document.getElementById("dailyBar").value == "princess") {
+
+                                let x = document.createElement("IMG");
+                                    x.setAttribute("src", "thr.jpg");
+                                    x.setAttribute("width", "1100");
+                                    x.setAttribute("height", "900");
+                                    x.setAttribute("alt", "The Pulpit Rock");
+                                    document.body.appendChild(x);
+
+                                return
+                            } else {
+                                
+                            }
+                            if (document.getElementById("dailyBar").value == "qbar"){
+                                let z = document.createElement("IMG");
+                                    z.setAttribute("src", "thr.jpg");
+                                    z.setAttribute("width", "1100");
+                                    z.setAttribute("height", "900");
+                                    z.setAttribute("alt", "The Pulpit Rock");
+                                    document.body.appendChild(z);
+                                return
+                            } else {
+                                console.log(" :) ");
+                            }
+                            if (document.getElementById("dailyBar").value == "Rock") {
+                                console.log("rock club");
+                                return
+                            } else {
+                                
+                            }
+
+
             }
             
